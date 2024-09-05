2ce3422b491541570bf14f992e1d8ecfc9d5d664
LucasYKoronoma <yoshioengcivil@gmail.com>
2024-09-04 21:57:21 -0300

    feat: Delete controller and view

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
-using System;
-using System.Collections.Generic;
+using SalesWebMvc.Models.ViewModels;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Controllers
 {
@@ -17,7 +13,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course.";
             ViewData["email"] = "yoshioengcivil@gmail.com";
 
             return View();
@@ -41,4 +37,5 @@ namespace SalesWebMvc.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+   
 }
